bd755a536d09c513308a39385c3d9cb7a9cf7bbb
thiagodeandrade <andrade.th@gmail.com>
2022-06-09 11:23:05 -0300

    Exemplos

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "modeloteste" {
     ami = "ami-0022f774911c1d690"
     instance_type = "t2.micro"
-    key_name = "quode_labaws"
+    key_name = "NOME_SUA_CHAVE_SSH_AS"
     vpc_security_group_ids = [aws_security_group.allow_services.id]
     tags = {
     Name    = "terraform-ec2"
